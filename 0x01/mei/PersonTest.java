39a16f1fd67642f225f26491a4f696e24e061f8e
Maikon Jakson <mjakson@ciandt.com>
2022-07-20 15:03:53 -0300

    Testes de software - JUnit - Parte1 _ TASK 1

---
+++ 0x01/mei/PersonTest.java
@@ -0,0 +1,44 @@
+import org.example.Person;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTest {
+    Person person = new Person();
+
+    @BeforeEach
+    public void setup() {
+        person.setBirthDate(1947);
+    }
+
+    @Test
+    void show_full_name() {
+        person.setName("Paul");
+        person.setSurname("McCartney");
+        assertEquals("Paul McCartney", person.fullName());
+    }
+
+    @Test
+    void test_calculateYearlySalary() {
+        person.setSalary(1200f);
+        assertEquals(14400.0, person.calculateYearlySalary());
+    }
+
+    @Test
+    void person_is_MEI(){
+        person.setAnotherCompanyOwner(false);
+        person.setPensioner(false);
+        person.setPublicServer(false);
+
+        assertTrue(person.isMEI());
+    }
+
+    @Test
+    void person_is_not_MEI() {
+        person.setAnotherCompanyOwner(true);
+        person.setPensioner(true);
+        person.setPublicServer(true);
+
+        assertFalse(person.isMEI());
+    }
+}
