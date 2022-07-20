aa0a0d2f7712dd60b3e1c7d46923b4ef759b7560
Maikon Jakson <107194793+MjaksonCiandT@users.noreply.github.com>
2022-07-20 13:10:06 -0300

    Update TesteEmail.java

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -1,5 +1,5 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TesteEmail {
 
@@ -15,7 +15,7 @@ public class TesteEmail {
         assertFalse(email.emailValid("email_testedominio.com.br"));
     }
 
-   @Test
+    @Test
     public void  testar_email_mais_50_caracteres(){
         Pessoa email = new Pessoa();
         assertEquals(Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"),false);
