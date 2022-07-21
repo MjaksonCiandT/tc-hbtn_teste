6c04e66d78a21c196b4b234716699d9bad193b68
Maikon Jakson <mjakson@ciandt.com>
2022-07-21 10:21:41 -0300

    Testes de software - JUnit - Parte 2 _ TASK 0

---
+++ 0x02/password/src/Person.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Person {
+
+    public static boolean checkUser(String usuario){
+        boolean contains = !usuario.contains("(?=.[}{,.^?~=+\\\\-_\\\\/\\\\-+.\\\\|])(?=.*[0-9]).{8,}\")");
+        if (usuario.length() >= 8 && contains){
+            return true ;
+        }
+        return false;
+    }
+
+    public static boolean checkPassword(String password){
+        if (password.matches("(?=.[}{,.^?~=+\\-_\\/\\-+.\\|])(?=.[a-zA-Z])(?=.[0-9]).{8,}")){
+            return true ;
+        }
+        return false;
+    }
+}
